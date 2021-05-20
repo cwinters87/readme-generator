dd8c07d45e9fbdd4305bdf6ece0cd91c296063e7
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-05-20 15:32:52 -0500

    added questions and prompt the user

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "How do you test this project"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What liscencse are you using",
+            choices: [
+                "Apache",
+                "Academic",
+                "GPL",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {
+
+//}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {  
+    inquirer.prompt(questions).then(data => {
+        console.log(data)
+    })
+    }
 
 // Function call to initialize app
 init();
