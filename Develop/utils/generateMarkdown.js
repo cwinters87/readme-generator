1bc586264164db2cfe5693968ee508427877eec4
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-05-21 00:11:27 -0500

    seeing if object literals will work instead

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,29 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  let badge;
-  switch(license) {
-    case "Apache 2.0":
-      badge = "[![badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-      break;
-    case "Boost Software 1.0":
-      badge = "[![badge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-      break;
-    case "Eclipse Public 1.0":
-      badge = "[![badge](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-      break;
-    case "MIT":
-      badge = "[![badge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      break;
-    case "Mozilla 2.0":
-      badge = "[![badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-      break;
-    case "None":
-      badge = "";
-      break;
-  }
-  return badge
+// function renderLicenseBadge(license) {
+//   let badge;
+//   switch(license) {
+//     case "Apache 2.0":
+//       badge = "[![badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+//       break;
+//     case "Boost Software 1.0":
+//       badge = "[![badge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+//       break;
+//     case "Eclipse Public 1.0":
+//       badge = "[![badge](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+//       break;
+//     case "MIT":
+//       badge = "[![badge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+//       break;
+//     case "Mozilla 2.0":
+//       badge = "[![badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+//       break;
+//     case "None":
+//       badge = "";
+//       break;
+//   }
+//   return badge
+// }
+
+function renderLicenseBadge(type) {
+  let badge = {
+
+
+    'Apache 2.0': function () {
+      return '[![badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    },
+
+
+
+    'Boost Software 1.0': function () {
+      return '[![badge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+    },
+
+
+
+    'Eclipse Public 1.0': function () {
+      return '[![badge](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+    },
+
+
+
+    'MIT': function () {
+      return '[![badge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    },
+
+
+
+    'Mozilla 2.0': function () {
+      return '[![badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    },
+
+
+
+    'None': function () {
+      return '';}
+
+
+
+    
+  };
+  return badge[type];
 }
+
+// let's call it
+    // var drink = getDrink('coke');
+    // console.log(drink); // 'Coke'
+
+
+
+
+
+
+
+    
 
 // Please see below!!!:
 //I have the badges themselves link to the license. I built it into the function above - Chris Winters
