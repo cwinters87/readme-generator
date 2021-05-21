af35dfffeaff2e366be06f772ea4b17c9ad1115c
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-05-21 01:44:53 -0500

    updated the readme using the generator itself

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,30 +1,6 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// function renderLicenseBadge(license) {
-//   let badge;
-//   switch(license) {
-//     case "Apache 2.0":
-//       badge = "[![badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-//       break;
-//     case "Boost Software 1.0":
-//       badge = "[![badge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-//       break;
-//     case "Eclipse Public 1.0":
-//       badge = "[![badge](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-//       break;
-//     case "MIT":
-//       badge = "[![badge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-//       break;
-//     case "Mozilla 2.0":
-//       badge = "[![badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-//       break;
-//     case "None":
-//       badge = "";
-//       break;
-//   }
-//   return badge
-// }
-
 function renderLicenseBadge(choice) {
   let badge = {
     'Apache 2.0': function () {
@@ -48,56 +24,18 @@ function renderLicenseBadge(choice) {
   return badge[choice]();
 }
 
-// let's call it
-    // var drink = getDrink('coke');
-    // console.log(drink); // 'Coke'
-
-
-
-
-
-
 
     
-
 // Please see below!!!:
-//I have the badges themselves link to the license. I built it into the function above - Chris Winters
+  //I have the badges themselves link to the license. I built it into the function above - Chris Winters
     // TODO: Create a function that returns the license link
     // If there is no license, return an empty string
     //function renderLicenseLink(license) {}
 
+
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   let section;
-//   switch(license) {
-//     case "Apache 2.0":
-//       section = `## License
-// [![badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) <br /> This application is covered by the ${license} license.`;
-//       break;
-//     case "Boost Software 1.0":
-//       section = `## License
-// [![badge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt) <br /> This application is covered by the ${license} license.`;
-//       break;
-//     case "Eclipse Public 1.0":
-//       section = `## License
-// [![badge](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0) <br /> This application is covered by the ${license} license.`;
-//       break;
-//     case "MIT":
-//       section = `## License
-// [![badge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br /> This application is covered by the ${license} license.`;
-//       break;
-//     case "Mozilla 2.0":
-//       section = `## License
-// [![badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0) <br /> This application is covered by the ${license} license.`;
-//       break;
-//     case "None":
-//       section = "";
-//       break;
-//   }
-//   return section
-// }
-
 function renderLicenseSection(choice) {
   let section = {
     'Apache 2.0': function () {
@@ -126,11 +64,6 @@ function renderLicenseSection(choice) {
   };
   return section[choice]();
 }
-
-
-
-
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
